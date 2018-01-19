e57a473a1291353275aeecf9fd88ab4203214b46
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-19 16:04:14 +0300

    add gtiignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/seth/Documents/moringa-core-projects/python/django/The-Moringa-Tribune/venv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
