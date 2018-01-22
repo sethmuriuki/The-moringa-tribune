52ce43f2a610bd21b43d19305b839e1ca8cb9923
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-22 15:13:25 +0300

    update views to save subscriber details

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from . models import Articles
 from .forms import NewsLetterForm
@@ -16,7 +16,12 @@ def news_today(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name, email = email)
+
+            recipient.save()
+            HttpResponseRedirect('news')
     else: 
         form = NewsLetterForm()
     
