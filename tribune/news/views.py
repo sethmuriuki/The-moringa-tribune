2cd4ec32e57cc246e784e4c07acb258045f002ee
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-22 11:52:58 +0300

    update views

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 from . models import Articles
+from .forms import NewsLetterForm
 
 # Create your views here.
 def welcome(request):
